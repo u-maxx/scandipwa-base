51ec45fb472e188be88a77cef4240c072c30c49d
Ilja Lapkovskis <ilja@scandi.com>
2019-04-04 22:21:08 +0300

    Initialize backstop js for regression testing

---
+++ deploy/backstopjs/backstop_data/html_report/config.js
@@ -0,0 +1,233 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_homepage_0_document_0_phone.png",
+        "label": "ScandiPWA homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 302
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_homepage_0_document_1_tablet.png",
+        "label": "ScandiPWA homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 269
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_homepage_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_homepage_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_homepage_0_document_2_desktop.png",
+        "label": "ScandiPWA homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 290
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_categorymen_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_categorymen_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_categorymen_0_document_0_phone.png",
+        "label": "ScandiPWA category:men",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com/category/men",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.02",
+          "analysisTime": 366
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_categorymen_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_categorymen_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_categorymen_0_document_1_tablet.png",
+        "label": "ScandiPWA category:men",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com/category/men",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.05",
+          "analysisTime": 372
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png",
+        "label": "ScandiPWA category:men",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com/category/men",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.72",
+          "analysisTime": 369
+        },
+        "diffImage": "../bitmaps_test/20190404-221927/failed_diff_backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_categorymen_0_document_0_phone.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_categorymen_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_categorymen_0_document_0_phone.png",
+        "label": "ScandiPWA category:men",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com/category/men",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.02",
+          "analysisTime": 347
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_categorymen_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_categorymen_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_categorymen_0_document_1_tablet.png",
+        "label": "ScandiPWA category:men",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com/category/men",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.05",
+          "analysisTime": 370
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png",
+        "test": "../bitmaps_test/20190404-221927/backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png",
+        "label": "ScandiPWA category:men",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://demo.scandipwa.com/category/men",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.72",
+          "analysisTime": 369
+        },
+        "diffImage": "../bitmaps_test/20190404-221927/failed_diff_backstop_default_ScandiPWA_categorymen_0_document_2_desktop.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
